52562e0c34957e59ce4ed8706b82fc37aa8766bd
Victor <krasilnikof@yandex.ru>
2022-04-10 14:03:52 +0300

    fix import path for gteetingByName() in bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import gteetingByName from '/Users/galinatopchiy/frontend-project-lvl1/src/cli.js';
+import gteetingByName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
