9df12f1c03f276f766cfebd429839af3aa263516
Victor <krasilnikof@yandex.ru>
2022-04-14 15:41:32 +0300

    fix "getRandomNumber"

--- src/random-number.js
+++ src/random-number.js
@@ -1,5 +1,5 @@
 const getRandomNumber = (min, max) => {
-  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   return randomNumber;
 };
 export default getRandomNumber;
