c64553dd153c60e0a45ef73c494479e3452f6765
Victor <krasilnikof@yandex.ru>
2022-04-14 15:47:01 +0300

    add function "decision"

--- games/brain-even-game.js
+++ games/brain-even-game.js
@@ -4,16 +4,11 @@ import getRandomNumber from '../src/random-number.js';
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const condition = () => {
-  const number = getRandomNumber(1, 100);
-  console.log(number);
-  return number;
+const decision = () => {
+  const condition = getRandomNumber(1, 100);
+  const isEven = condition % 2 === 0;
+  const correctAnswer = isEven ? 'yes' : 'no';
+  return [condition, correctAnswer];
 };
 
-const isEven = condition % 2 === 0;
-
-const correctAnswer = isEven ? 'yes' : 'no';
-
-const startEven = commonLogics(task, condition, correctAnswer);
-
-export default startEven;
+export default () => commonLogics(task, decision);
