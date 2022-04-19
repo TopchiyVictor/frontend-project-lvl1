10cb06eed76ab0819f5b70d143a3e5ff4be71145
Victor <krasilnikof@yandex.ru>
2022-04-19 19:34:05 +0300

    rename funktion "howManyQuestions" in "maxRounds"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable comma-dangle */
 import readlineSync from 'readline-sync';
 
-const howManyQuestions = 3;
+const maxRounds = 3;
 
 const commonLogics = (task, decision) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(task);
-  for (let i = 1; i <= howManyQuestions; i += 1) {
+  for (let i = 1; i <= maxRounds; i += 1) {
     const [condition, correctAnswer] = decision();
     console.log(`Question: ${condition}`);
     const answer = readlineSync.question('Your answer: ');
