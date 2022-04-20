9dbcb3930dd5d0cc4a6ef46e89a0d99ebb0da3c7
Victor <krasilnikof@yandex.ru>
2022-04-20 10:56:09 +0300

    rename function in brain-even-game.js

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -4,11 +4,11 @@ import getRandomNumber from '../random-number.js';
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const decision = () => {
+const getRoundData = () => {
   const condition = getRandomNumber(1, 100);
   const isEven = condition % 2 === 0;
   const correctAnswer = isEven ? 'yes' : 'no';
   return [condition, correctAnswer];
 };
 
-export default () => commonLogics(task, decision);
+export default () => commonLogics(task, getRoundData);
