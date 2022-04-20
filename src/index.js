c1107cce9d412635507267cce601bc019d10004e
Victor <krasilnikof@yandex.ru>
2022-04-20 11:01:34 +0300

    rename function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-/* eslint-disable comma-dangle */
 import readlineSync from 'readline-sync';
 
 const maxRounds = 3;
 
-const commonLogics = (task, decision) => {
+const commonLogics = (task, getRoundData) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(task);
-  for (let i = 1; i <= maxRounds; i += 1) {
-    const [condition, correctAnswer] = decision();
+  for (let i = 0; i < maxRounds; i += 1) {
+    const [condition, correctAnswer] = getRoundData();
     console.log(`Question: ${condition}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === correctAnswer) {
