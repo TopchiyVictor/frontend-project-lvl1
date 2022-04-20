ab2d0ec60785aa7fc8dcc55356ea78556399fa7d
Victor <krasilnikof@yandex.ru>
2022-04-20 11:00:35 +0300

    add function "getProgression" in src/games/brain-progression-game.js

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -4,21 +4,26 @@ import commonLogics from '../index.js';
 
 const task = 'What number is missing in the progression?';
 
-const decision = () => {
+const getProgression = () => {
   const increaseProgression = getRandomNumber(1, 5);
   const startNumProgression = getRandomNumber(1, 100);
   let nextStepProgression = startNumProgression;
   const numOfDigitInProgression = getRandomNumber(5, 10);
   const progression = [];
-  for (let i = 1; i <= numOfDigitInProgression; i += 1) {
+  for (let i = 0; i < numOfDigitInProgression; i += 1) {
     progression.push(`${nextStepProgression}`);
     nextStepProgression += increaseProgression;
   }
-  const indexHiddenNum = getRandomNumber(0, numOfDigitInProgression - 1);
+  return progression;
+};
+
+const getRoundData = () => {
+  const progression = getProgression();
+  const indexHiddenNum = getRandomNumber(0, progression.length - 1);
   const correctAnswer = progression[indexHiddenNum];
   progression[indexHiddenNum] = '..';
   const condition = progression.join(' ');
   return [condition, correctAnswer];
 };
 
-export default () => commonLogics(task, decision);
+export default () => commonLogics(task, getRoundData);
